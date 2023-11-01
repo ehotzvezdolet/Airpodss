f6f1555030ee40d7f23786501639f34383b29929
Igorek <ehotzvezdolet@gmail.com>
2023-11-01 19:43:42 +0100

    finished

--- source/index.js
+++ source/index.js
@@ -1,66 +1,81 @@
 document.addEventListener('DOMContentLoaded', function () {
- const bannerList = document.querySelectorAll('.banner__wrapper');
- const optionsListItems = document.querySelectorAll('.options__list > li');
- const optionsList = document.querySelector('.options__list');
- const descriptionImageList = document.querySelectorAll('.description__image > img'); 
- const advantagesImageList = document.querySelectorAll('.advantages__image > img'); 
+  //burger menu
+  if (document.querySelectorAll('.header__nav').length) {
+    const nav = document.querySelector('.header__nav'),
+      btn = document.querySelector('.header__burger'),
+      menu = document.querySelector('.header__nav_mobile'),
+      body = document.body;
 
- // add artibute for banner
- for (let i = 0; i < bannerList.length; i++) {
-  const bannerListAttr = bannerList[i].setAttribute('data-content', i);
- }
-
- // add atribute for option button
- for (let i = 0; i < optionsListItems.length; i++) {
-  const optionsListAttr = optionsListItems[i].setAttribute('data-content', i);
- }
-
- // add atribute for description image
- for (let i=0; i < descriptionImageList.length; i++) {
-  const descriptionImageListAttr = descriptionImageList[i].setAttribute('data-content', i);
- }
-
- // add atribute for advantages image
- for (let i=0; i < advantagesImageList.length; i++) {
-  const advantagesImageListAttr = advantagesImageList[i].setAttribute('data-content', i);
- }
-
- // choose options button
- optionsListItems.forEach(option => {
-  option.addEventListener('click', e => {
-   if (!e.currentTarget.classList.contains('choosed')) {
-    optionsListItems.forEach(i => {
-     if (i.classList.contains('choosed')) i.classList.remove('choosed');
-    })
-    e.currentTarget.classList.add('choosed');
-    // hide effect optionsList
-    if (optionsList.classList.contains('effect')) optionsList.classList.remove('effect');
-    function effect() {
-     optionsList.classList.add('effect');
-    };
-    setTimeout(effect, 10);
-    // change banner
-    const optionAttr = e.currentTarget.getAttribute('data-content');
-    bannerList.forEach(banner => {
-     const bannerAttr = banner.getAttribute('data-content');
-     if (optionAttr == bannerAttr) banner.classList.add('choosed');
-     else banner.classList.remove('choosed');
+    btn.addEventListener('click', function () {
+      btn.classList.toggle('active');
+      menu.classList.toggle('active');
+      body.classList.toggle('hidden');
     });
-    // change description image
-    descriptionImageList.forEach(image => {
-     const imageAttr = image.getAttribute('data-content');
-     if (optionAttr == imageAttr) image.classList.add('choosed');
-     else image.classList.remove('choosed');
+  }
+
+
+  const bannerList = document.querySelectorAll('.banner__wrapper');
+  const optionsListItems = document.querySelectorAll('.options__list > li');
+  const optionsList = document.querySelector('.options__list');
+  const descriptionImageList = document.querySelectorAll('.description__image > img');
+  const advantagesImageList = document.querySelectorAll('.advantages__image > img');
+
+  // add artibute for banner
+  for (let i = 0; i < bannerList.length; i++) {
+    const bannerListAttr = bannerList[i].setAttribute('data-content', i);
+  }
+
+  // add atribute for option button
+  for (let i = 0; i < optionsListItems.length; i++) {
+    const optionsListAttr = optionsListItems[i].setAttribute('data-content', i);
+  }
+
+  // add atribute for description image
+  for (let i = 0; i < descriptionImageList.length; i++) {
+    const descriptionImageListAttr = descriptionImageList[i].setAttribute('data-content', i);
+  }
+
+  // add atribute for advantages image
+  for (let i = 0; i < advantagesImageList.length; i++) {
+    const advantagesImageListAttr = advantagesImageList[i].setAttribute('data-content', i);
+  }
+
+  // choose options button
+  optionsListItems.forEach(option => {
+    option.addEventListener('click', e => {
+      if (!e.currentTarget.classList.contains('choosed')) {
+        optionsListItems.forEach(i => {
+          if (i.classList.contains('choosed')) i.classList.remove('choosed');
+        })
+        e.currentTarget.classList.add('choosed');
+        // hide effect optionsList
+        if (optionsList.classList.contains('effect')) optionsList.classList.remove('effect');
+        function effect() {
+          optionsList.classList.add('effect');
+        };
+        setTimeout(effect, 10);
+        // change banner
+        const optionAttr = e.currentTarget.getAttribute('data-content');
+        bannerList.forEach(banner => {
+          const bannerAttr = banner.getAttribute('data-content');
+          if (optionAttr == bannerAttr) banner.classList.add('choosed');
+          else banner.classList.remove('choosed');
+        });
+        // change description image
+        descriptionImageList.forEach(image => {
+          const imageAttr = image.getAttribute('data-content');
+          if (optionAttr == imageAttr) image.classList.add('choosed');
+          else image.classList.remove('choosed');
+        });
+        // change advantages image
+        advantagesImageList.forEach(image => {
+          const imageAttr = image.getAttribute('data-content');
+          if (optionAttr == imageAttr) image.classList.add('choosed');
+          else image.classList.remove('choosed');
+        });
+      }
     });
-    // change advantages image
-    advantagesImageList.forEach(image => {
-     const imageAttr = image.getAttribute('data-content');
-     if (optionAttr == imageAttr) image.classList.add('choosed');
-     else image.classList.remove('choosed');
-    });
-   }
-  });
- })
+  })
 
 
 
